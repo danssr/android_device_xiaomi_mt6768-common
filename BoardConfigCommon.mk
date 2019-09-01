90a23e3e565d240f63e21c9442c6ffe5cedaf8d1
Danny Lin <danny@kdrag0n.dev>
2019-09-01 01:17:58 -0700

    mt6768-common: cmdline: Disable unhandled or broken IRQ monitoring
    This is primarily intended for generic x86 kernels which may run on all
    sorts of broken systems. Our kernel runs on known hardware, so this is
    unnecessary.
    
    Disable it for a minor IRQ handler overhead reduction.
    
    Suggested-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Change-Id: I1992d2c88d3f3b9a9d15748d6c3c6bf6709d8812

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,6 +64,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.serialconsole=0
 BOARD_KERNEL_CMDLINE += nodebugmon
+BOARD_KERNEL_CMDLINE += noirqdebug
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
