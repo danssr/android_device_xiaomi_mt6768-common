0dbf3dfb5f29b2c68cf2306ccf1f264a092c6634
kdrag0n <dragon@khronodragon.com>
2022-06-19 14:07:36 +0100

    mt6768-common: cmdline: Disable debug monitoring

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,6 +63,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += androidboot.serialconsole=0
+BOARD_KERNEL_CMDLINE += nodebugmon
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
